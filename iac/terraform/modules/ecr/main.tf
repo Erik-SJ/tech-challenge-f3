08ad8725649f0ac208890c5da6cfa8a3fc23b136
Erik Jacob <erik.sjacob@gmail.com>
2026-08-12 15:27:40 -0300

    ci: update service URLs to empty strings for evaluation, flag, and targeting services

--- iac/terraform/modules/ecr/main.tf
+++ iac/terraform/modules/ecr/main.tf
@@ -2,16 +2,14 @@ resource "aws_ecr_repository" "service" {
 
   for_each = toset(local.repositories)
 
-  name = each.value
-
+  name                 = each.value
   image_tag_mutability = "IMMUTABLE"
-
+  force_delete         = true
   image_scanning_configuration {
 
     scan_on_push = true
 
   }
-
   encryption_configuration {
 
     encryption_type = "AES256"
--- iac/terraform/modules/eks/main.tf
+++ iac/terraform/modules/eks/main.tf
@@ -118,7 +118,7 @@ resource "aws_eks_node_group" "this" {
 
   scaling_config {
 
-    desired_size = 1
+    desired_size = 2
     min_size     = 1
     max_size     = 2
 
